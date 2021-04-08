85dd846fd43ab3e36d01d5c5de824987b2a05bc8
Thulio Bittencourt <bittencourtthulio@gmail.com>
2021-04-08 10:35:59 -0300

    Update AWS4D.S3.pas

--- src/AWS4D.S3.pas
+++ src/AWS4D.S3.pas
@@ -1,7 +1,5 @@
 unit AWS4D.S3;
-
 interface
-
 uses
   System.Classes,
   AWS4D.Interfaces,
@@ -11,7 +9,6 @@ uses
   {$ELSE}
     Vcl.ExtCtrls;
   {$ENDIF}
-
 type
   TAWS4DS3 = class(TInterfacedObject, iAWS4DS3)
     private
@@ -19,7 +16,6 @@ type
       FParent : iAWS4D;
       FContent : String;
       FContentByteStream : TBytesStream;
-      FCredencial : iAWS4DCredential;
     public
       constructor Create(aParent : iAWS4D);
       destructor Destroy; override;
@@ -34,45 +30,36 @@ type
       function ContentByteStream : TBytesStream;
       function &End : iAWS4D;
    end;
-
 implementation
-
 uses
   Data.Cloud.AmazonAPI,
   Data.Cloud.CloudAPI,
   AWS4D.S3.Get,
   AWS4D.S3.Send,
   AWS4D.S3.Credencial;
-
 { TBind4DAmazonS3 }
-
 function TAWS4DS3.ToBytesStream: TBytesStream;
 begin
   Result := FContentByteStream;
 end;
-
 function TAWS4DS3.ToString: String;
 begin
   Result := FContent;
 end;
-
 function TAWS4DS3.Content(aValue: String): iAWS4DS3;
 begin
   Result := Self;
   FContent := aValue;
 end;
-
 function TAWS4DS3.&End: iAWS4D;
 begin
   Result := FParent;
 end;
-
 function TAWS4DS3.Content(var aValue: TBytesStream): iAWS4DS3;
 begin
   Result := Self;
   FContentByteStream := aValue;
 end;
-
 function TAWS4DS3.ContentByteStream: TBytesStream;
 begin
   Result := FContentByteStream;
@@ -83,15 +70,12 @@ begin
   FParent := aParent;
   FContentByteStream := TBytesStream.Create;
 end;
-
 destructor TAWS4DS3.Destroy;
 begin
   if Assigned(FContentByteStream) then
     FContentByteStream.Free;
-
   inherited;
 end;
-
 function TAWS4DS3.FromImage(var aValue: TImage): iAWS4DS3;
 var
   FJpegImage : TJpegImage;
@@ -109,21 +93,17 @@ begin
     FJpegImage.Free;
   end;
 end;
-
 function TAWS4DS3.GetFile: iAWS4DS3GetFile;
 begin
   Result := TAWS4DS3GetFile.New(Self);
 end;
-
 class function TAWS4DS3.New(aParent : iAWS4D) : iAWS4DS3;
 begin
   Result := Self.Create(aParent);
 end;
-
 function TAWS4DS3.SendFile: iAWS4DS3SendFile;
 begin
   Result := TAWS4DS3SendFile.New(Self);;
 end;
-
 
 end.
